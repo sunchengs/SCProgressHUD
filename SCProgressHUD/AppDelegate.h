8ea5f1369c8566e939b4618df5ec4e50dbc51c7e
孙程 <277469661@qq.com>
2016-07-14 16:12:01 +0800

    Initial Commit

---
+++ SCProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCProgressHUD
+//
+//  Created by 孙程 on 16/7/14.
+//  Copyright © 2016年 Suncheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCProgressHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCProgressHUD
+//
+//  Created by 孙程 on 16/7/14.
+//  Copyright © 2016年 Suncheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
